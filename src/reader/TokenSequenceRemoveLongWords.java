bd9cbd675c8bf644726c741496462305fb034e4c
Darren Foong <darrenfoong@gmail.com>
2015-12-25 21:19:52 +0800

    Add TokenSequenceRemoveLongWords
    Not enabled in any Reader yet, because testing shows that it is not very
    effective in reducing the number of features.

---
+++ src/reader/TokenSequenceRemoveLongWords.java
@@ -0,0 +1,33 @@
+package reader;
+
+import java.util.Iterator;
+
+import cc.mallet.pipe.Pipe;
+import cc.mallet.types.Instance;
+import cc.mallet.types.Token;
+import cc.mallet.types.TokenSequence;
+
+public class TokenSequenceRemoveLongWords extends Pipe {
+	private static final long serialVersionUID = 1L;
+
+	private int maxLength;
+
+	public TokenSequenceRemoveLongWords(int maxLength) {
+		this.maxLength = maxLength;
+	}
+
+	@Override
+	public Instance pipe(Instance inst) {
+		TokenSequence ts = (TokenSequence) inst.getData();
+
+		Iterator<Token> iter = ts.iterator();
+
+		while ( iter.hasNext() ) {
+			if ( iter.next().getText().length() > maxLength ) {
+				iter.remove();
+			}
+		}
+
+		return inst;
+	}
+}
