65a322ad3e60b652b4e6459596d0e258ce800f4f
Darren Foong <darrenfoong@gmail.com>
2015-12-28 22:53:50 +0800

    Refactor NaiveBayesClassifier
    Make feature cutoff and lambda command line arguments; disable
    generic n-fold cross-validation routine.

--- src/NaiveBayesClassifier.java
+++ src/NaiveBayesClassifier.java
@@ -10,16 +10,12 @@ import utils.Statistics;
 import cc.mallet.classify.Classification;
 import cc.mallet.classify.NaiveBayes;
 import cc.mallet.classify.NaiveBayesTrainer;
-import cc.mallet.types.CrossValidationIterator;
 import cc.mallet.types.InstanceList;
 import cc.mallet.types.Multinomial;
 
 public class NaiveBayesClassifier {
-	private static final int NFOLDS = 10;
-	private static final int CUTOFF = 50;
-	private static final double LAMBDA = 1;
 	private static final int[] NS = null;
-	private static final boolean SMOOTHING = true;
+	private static final boolean SMOOTHING = false;
 
 	private static CorpusReader corpusReader = null;
 	private static String directory;
@@ -28,65 +24,50 @@ public class NaiveBayesClassifier {
 
 	public static void main(String[] args) {
 		String corpus = args[0];
-		String subset = "lemm_stop";
+		String subset = args[1];
+		int cutoff = Integer.parseInt(args[2]);
+		double lambda = Double.parseDouble(args[3]);
 		boolean stop = false;
 
 		switch (corpus) {
 			case "pu1":
-				if ( args.length == 2 ) {
-					subset = args[1]; // bare, lemm, lemm_stop, stop
-					System.out.println("Subset " + subset + " used.");
-				} else {
-					System.out.println("No subset of PU1 selected: subset " + subset + " used.");
-				}
+				System.out.println("Subset " + subset + " used.");
 
-				corpusReader = new PuReader(CUTOFF, NS);
+				corpusReader = new PuReader(cutoff, NS);
 				directory = "data/pu1/" + subset + "/";
 				System.out.println("Starting PU1 reader.");
 				break;
 			case "genspam":
-				if ( args.length == 2 ) {
-					subset = args[1]; // bare, lemm, lemm_stop, stop
-
-					if ( subset == "lemm_stop" ) {
-						subset = "lemm";
-						stop = true;
-					}
-
-					if ( subset == "stop" ) {
-						subset = "bare";
-						stop = true;
-					}
-
-					System.out.println("Subset " + subset + " used.");
-				} else {
-					System.out.println("No subset of GenSpam selected: subset " + subset + " used.");
+				if ( subset == "lemm_stop" ) {
+					subset = "lemm";
+					stop = true;
 				}
 
-				corpusReader = new GenSpamReader(CUTOFF, NS, stop);
+				if ( subset == "stop" ) {
+					subset = "bare";
+					stop = true;
+				}
+
+				System.out.println("Subset " + subset + " used.");
+
+				corpusReader = new GenSpamReader(cutoff, NS, stop);
 				directory = "data/genspam/" + subset + "/";
 				System.out.println("Starting GenSpam reader.");
 				break;
 			case "trec07p":
-				if ( args.length == 2 ) {
-					subset = args[1]; // bare, lemm, lemm_stop, stop
-
-					if ( subset == "lemm_stop" ) {
-						subset = "lemm";
-						stop = true;
-					}
-
-					if ( subset == "stop" ) {
-						subset = "bare";
-						stop = true;
-					}
-
-					System.out.println("Subset " + subset + " used.");
-				} else {
-					System.out.println("No subset of TREC 2007 selected: subset " + subset + " used.");
+				if ( subset == "lemm_stop" ) {
+					subset = "lemm";
+					stop = true;
 				}
 
-				corpusReader = new TrecReader(CUTOFF, NS, stop);
+				if ( subset == "stop" ) {
+					subset = "bare";
+					stop = true;
+				}
+
+				System.out.println("Subset " + subset + " used.");
+
+				corpusReader = new TrecReader(cutoff, NS, stop);
 				directory = "data/trec07p/";
 				System.out.println("Starting TREC 2007 reader.");
 				break;
@@ -120,10 +101,10 @@ public class NaiveBayesClassifier {
 
 				File testDirectory = new File(directory + "part" + i + "/");
 
-				runNaiveBayes(trainDirectoriesArray, new File[] {testDirectory});
+				runNaiveBayes(trainDirectoriesArray, new File[] {testDirectory}, lambda);
 			}
 
-			Evaluator evaluator = new Evaluator(LAMBDA,
+			Evaluator evaluator = new Evaluator(lambda,
 									(int) stats.getSum("totalSpam"),
 									(int) stats.getSum("totalHam"),
 									(int) stats.getSum("spamToSpam"),
@@ -134,8 +115,12 @@ public class NaiveBayesClassifier {
 			System.out.println(evaluator.getRawStats());
 			System.out.println(evaluator.getStats());
 
+			System.out.println();
+
+			System.out.println("Macroaverage spam precision: " + stats.getAverage("sp"));
+			System.out.println("Macroaverage spam recall: " + stats.getAverage("sr"));
+			System.out.println("Macroaverage weighted accuracy: " + stats.getAverage("wacc"));
 			System.out.println("Macroaverage TCR: " + stats.getAverage("tcr"));
-			System.out.println("Microaverage TCR: " + evaluator.getTotalCostRatio());
 
 			return;
 		}
@@ -144,11 +129,12 @@ public class NaiveBayesClassifier {
 			File trainDirectory = new File(directory + "train/");
 			File testDirectory = new File(directory + "test/");
 
-			runNaiveBayes(new File[] {trainDirectory}, new File[] {testDirectory});
+			runNaiveBayes(new File[] {trainDirectory}, new File[] {testDirectory}, lambda);
 
 			return;
 		}
 
+		/*
 		corpusReader.readFeatures(new File[] {new File(directory)});
 		corpusReader.computeMutualInfo();
 		corpusReader.pruneAlphabet();
@@ -189,9 +175,10 @@ public class NaiveBayesClassifier {
 		}
 
 		System.out.println("Average TCR: " + stats.getAverage("tcr"));
+		*/
 	}
 
-	private static void runNaiveBayes(File[] trainDirectoriesArray, File[] testDirectoriesArray) {
+	private static void runNaiveBayes(File[] trainDirectoriesArray, File[] testDirectoriesArray, double lambda) {
 		corpusReader.readFeatures(trainDirectoriesArray);
 		corpusReader.computeMutualInfo();
 		corpusReader.pruneAlphabet();
@@ -215,10 +202,13 @@ public class NaiveBayesClassifier {
 		NaiveBayes nbClassifier = nbTrainer.getClassifier();
 
 		ArrayList<Classification> results = nbClassifier.classify(dataInstances);
-		Evaluator evaluator = new Evaluator(LAMBDA, results);
-		System.out.println("Evaluator initialised with lambda = " + LAMBDA);
+		Evaluator evaluator = new Evaluator(lambda, results);
+		System.out.println("Evaluator initialised with lambda = " + lambda);
 
 		stats.put("tcr", evaluator.getTotalCostRatio());
+		stats.put("sp", evaluator.getSpamPrecision());
+		stats.put("sr", evaluator.getSpamRecall());
+		stats.put("wacc", evaluator.getWeightedAccuracy());
 
 		stats.put("totalSpam", evaluator.getTotalSpam());
 		stats.put("totalHam", evaluator.getTotalHam());
